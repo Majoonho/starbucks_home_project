d123edb4a017d4fc6e1bb2922f8f80b076b2b935
Majoonho <jofmjh@gmail.com>
2022-12-28 20:35:19 +0900

    feat: gsap.to를 이용한 그림순차적으로 나타내기

--- js/main.js
+++ js/main.js
@@ -16,8 +16,12 @@ searchInputEl.addEventListener('blur', function (){
   searchInputEl.setAttribute('placeholder','');
 });
 
+
+
+// 스크롤 하면 배지 숨기는 기능 추가
 const badgeEl = document.querySelector('header .badges');
 console.log(badgeEl)
+
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
@@ -32,7 +36,29 @@ window.addEventListener('scroll', _.throttle(function () {
   //배지 보이기
   gsap.to(badgeEl, .6, {
     opacity: 1,
-    dispaly: 'block'})
-  };
+    dispaly: 'block'
+  });
+  }
   }, 300));
+
+
+// section 스타벅스 그림 순차적으로 나타내기
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+console.log(fadeEls)
+fadeEls.forEach(function(fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .6, //0.7 1.4 2.1 2.7 초후에 나오게 됨.
+    opacity: 1
+
+  });
+
+});
+
+
+
+
+
+
+
 
